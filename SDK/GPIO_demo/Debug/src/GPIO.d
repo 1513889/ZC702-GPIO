2fce010c33fa761b53cd8bd6440bd68e5574e3df
killer <vuductuan01031997@gmail.com>
2018-08-22 00:22:21 +0700

    upload code

---
+++ SDK/GPIO_demo/Debug/src/GPIO.d
@@ -0,0 +1,40 @@
+src/GPIO.o src/GPIO.d: ../src/GPIO.c \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../GPIO_demo_bsp/ps7_cortexa9_0/include/xparameters.h:
